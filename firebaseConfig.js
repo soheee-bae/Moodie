98b2fee62a6225c7169bfddaac0a17a83d302a2b
baesohee <sohee@sagetap.io>
2023-05-03 15:03:44 -0800

    initial setup

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyDyU8agQCcaA1Lrd_J5tG3oTdaUmq8_Ilk",
+  authDomain: "moodie-1a369.firebaseapp.com",
+  projectId: "moodie-1a369",
+  storageBucket: "moodie-1a369.appspot.com",
+  messagingSenderId: "252813573462",
+  appId: "1:252813573462:web:339ec0ae1ec454f89d2af7",
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIRESTORE_DB = getFirestore(FIREBASE_APP);
+export const FIREBASE_AUTH = getAuth(FIREBASE_APP);
